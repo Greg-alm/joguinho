18d41ccca91dd058fcbff759c353aae0e5d7450b
Greg-alm <a.gregory@escola.pr.gov.br>
2026-05-21 07:47:32 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -12,3 +12,25 @@ function lutar(){
     const dado = Math.floor(Math.random()*20)+1;
     let reultado = `DADO:${dado} |  ${classe} com ${arma}`;
 }
+switch(classe){
+        case "guerreiro":
+            if(arma.toLowerCase()==="espada" && dado>5){
+                resultado += "<span class='sucesso'> SUCESSO: Você apunhalou o monstro com sua espada.</span>"
+            }else if(dado>15){
+                resultado += "<span class='sucesso'> VITÓRIA: Você espancou o inimigo até a morte.</span>"
+            }else{
+                resultado += "<span class='derrota'> DERROTA: Você falhou Guerreiro, com a sua morte os monstros dizimaram a humanidade</span>"
+            }
+            break;
+
+        case "mago":
+            if(arma.toLowerCase()==="cajado" && dado>8){
+                resultado += "<span class='sucesso'> SUCESSO: Você lançou um feitiço no monstro.</span>"
+            }else if(dado>20){
+                resultado += "<span class='sucesso'> VITÓRIA: A sorte está do seu lado. Você venceu mesmo sem seus feitiços.</span>"
+            }else{
+                resultado += "<span class='derrota'> DERROTA: Você falhou miserávelmente. Resoltando em sua morte.</span>"
+            }
+            break;
+    }
+}
